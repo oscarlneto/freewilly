30c3ac76b32b8913f78a458a713e19cb3b8fda16
Oscar C. Lima Neto <oscarlnetoo@gmail.com>
2016-06-20 05:30:37 -0300

    updatepost ok

---
+++ freewilly-sails/assets/js/angular/updatePostController.js
@@ -0,0 +1,44 @@
+myApp.controller('updatePostController', ['$scope', '$http', '$window', function($scope, $http, $window) {
+
+  $scope.post = {};
+
+  $scope.loadPost = function(){
+
+    var post = {};
+    post.idPost = getParameterByName('idpost');
+
+    $http.post('http://localhost:1337/post/get', post).then(function(response) {
+
+      if(response.data.sucesso == true) {
+        $scope.post = response.data.rows[0];
+        console.log($scope.post);
+      }
+    });
+  }
+
+  $scope.removePost = function(){
+    
+    var post = {};
+    post.idPost = getParameterByName('idpost');
+
+    $http.post('http://localhost:1337/post/remove', post).then(function(response) {
+      $window.location.href = 'http://localhost:1337/freewilly/profile';
+    });
+  }
+
+  $scope.updatePost = function(){
+  
+    var post = {};
+    post.idPost = getParameterByName('idpost');
+    post.usuario = sessionStorage.getItem('usuario');
+    post.titulo = $('#postTitle').val();
+    post.conteudo = $('#postContent').val();
+    post.idGrupo =  $scope.post.idgrupo;
+
+    $http.post('http://localhost:1337/post/set', post).then(function(response) {
+      $window.location.href = 'http://localhost:1337/freewilly/profile';
+    });
+
+  }
+
+}]);
