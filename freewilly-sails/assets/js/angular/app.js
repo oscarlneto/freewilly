e9e5ad1d9194f66cb373da6bf47f5e71085387ca
talesprates <talesprates@hotmail.com>
2016-06-20 05:25:10 -0300

    Like and Share posts done

--- freewilly-sails/assets/js/angular/app.js
+++ freewilly-sails/assets/js/angular/app.js
@@ -24,12 +24,79 @@ myApp.controller('myPostsController', ['$scope', '$http', function($scope, $http
   $scope.posts = {};
 
   $scope.like = function(index){
-    $scope.posts[index].liked = true;
+    $scope.posts[index].reacao = 1;
+
+    var postReacao = {};
+
+    postReacao.idPost = $scope.posts[index].idpost;
+    postReacao.usuario = sessionStorage.getItem('usuario');
+    postReacao.reacao = 1;
+    postReacao.compartilhou = $scope.posts[index].compartilhou;
+
+    $http.post("http://localhost:1337/postreacao/create", postReacao).then(function(response) {
+      if(response.data.sucesso){
+        console.log('sucesso');
+      } else {
+        $http.post("http://localhost:1337/postreacao/set", postReacao).then(function(res) {
+          if(res.data.sucesso)
+            console.log('sucesso no set');
+        });
+      }
+    });
+
   }
 
   $scope.unlike = function(index){
-    $scope.posts[index].liked = false;
+    $scope.posts[index].reacao = 0;
+
+    var postReacao = {};
+
+    postReacao.idPost = $scope.posts[index].idpost;
+    postReacao.usuario = sessionStorage.getItem('usuario');
+    postReacao.reacao = 0;
+    postReacao.compartilhou = $scope.posts[index].compartilhou;
+
+    $http.post("http://localhost:1337/postreacao/set", postReacao).then(function(response) {
+      if(response.data.sucesso)
+        console.log('sucesso');
+    });
+
   }
+
+  $scope.share = function(index){
+    $scope.posts[index].compartilhou = true;
+
+    var postReacao = {};
+
+    postReacao.idPost = $scope.posts[index].idpost;
+    postReacao.usuario = sessionStorage.getItem('usuario');
+    postReacao.reacao = $scope.posts[index].reacao;
+    postReacao.compartilhou = true;
+
+    $http.post("http://localhost:1337/postreacao/create", postReacao).then(function(response) {
+      if(response.data.sucesso){
+        console.log('sucesso');
+      } else {
+        $http.post("http://localhost:1337/postreacao/set", postReacao).then(function(res) {
+          if(res.data.sucesso)
+            console.log('sucesso no set');
+        });
+      }
+    });
+
+    var post = {};
+    post.usuario = sessionStorage.getItem('usuario');
+    //post.titulo = ($scope.posts[index].titulo).concat("shared from <a href=/freewilly/user?username=", $scope.posts[index].usuario, ">",  $scope.posts[index].usuario, "</a>");
+    post.titulo = $scope.posts[index].titulo;
+    post.conteudo = $scope.posts[index].conteudo + " (shared from @" + $scope.posts[index].usuario + ")";
+
+    $http.post("http://localhost:1337/post/create", post).then(function(response) {
+      if(response.data.sucesso)
+        console.log('compartilhado');
+    });
+  }
+
+
 
   $scope.loadPosts = function(){
     var usuario = {usuario: sessionStorage.getItem('usuario')};
@@ -39,10 +106,11 @@ myApp.controller('myPostsController', ['$scope', '$http', function($scope, $http
   }
 
   $scope.loadUserPosts = function(){
-    var usuario = {usuario: getParameterByName('username')};
-    $http.post("http://localhost:1337/post/getByUsuario", usuario).then(function(response) {
+    var usuario = {usuario: sessionStorage.getItem('usuario'), follow: getParameterByName('username')};
+    $http.post("http://localhost:1337/post/getByUsuarioFollowReacao", usuario).then(function(response) {
       $scope.posts = response.data.rows;
     });
+
   }
 
 }]);
@@ -385,6 +453,7 @@ myApp.controller('profileController', ['$scope', '$http', '$window', 'authentica
       $scope.usuario = response.data.rows[0];
       console.log($scope.usuario);
     });
+
 
     usuario.usuario = sessionStorage.getItem("usuario");
     usuario.follow = getParameterByName('username');
